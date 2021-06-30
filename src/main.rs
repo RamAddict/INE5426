2e9b8e78efbf7f4c93172064fcfeea40e1b456e3
Enzo Albornoz <enzocoelhoalbornoz@gmail.com>
2021-06-30 12:59:42 -0300

    Remake

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+mod test;
+
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+struct ParserCC20211;
+
+fn main() {
+    println!("Hello, world!");
+    let fields = ParserCC20211::parse(Rule::program, "10 + 5").unwrap();
+    for field in fields {
+        println!("{}", field.as_span().as_str())
+    }
+}
